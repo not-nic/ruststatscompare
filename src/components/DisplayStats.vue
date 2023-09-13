ca637bec378b2a0d4be11af4ae90079a82e9210f
notnic <nicovo989@gmail.com>
2023-09-13 01:29:41 +0100

    Add FarmingStats.vue to DisplayStats.vue component.

--- src/components/DisplayStats.vue
+++ src/components/DisplayStats.vue
@@ -3,30 +3,26 @@ import {defineComponent} from 'vue'
 import {useStatStore} from "../stores/StatStore.ts";
 import UserProfile from "./UserProfile.vue";
 import PvpStats from "./PvpStats.vue";
-
-interface Stat {
-  name: string;
-  value: number;
-}
+import FarmingStats from "./FarmingStats.vue";
 
 export default defineComponent({
   name: "DisplayStats",
-  components: {PvpStats, UserProfile},
+  components: {FarmingStats,PvpStats, UserProfile},
+
+  data() {
+    return {
+      store: useStatStore().userSteamStats,
+    }
+  },
 
   setup() {
     const statStore = useStatStore();
-
     return {
       statStore,
     };
   },
-
-  methods: {
-
-  }
 })
 </script>
-
 
 <template>
   <div class="user" v-for="(userStats, index) in statStore.userSteamStats" :key="index">
@@ -40,12 +36,22 @@ export default defineComponent({
     ></user-profile>
     <div class="stats">
       <pvp-stats
-          :kills="statStore.getStatByKey(statStore.userSteamStats, index, 'kill_player')"
-          :deaths="statStore.getStatByKey(statStore.userSteamStats, index, 'deaths')"
-          :bullets-fired="statStore.getStatByKey(statStore.userSteamStats, index, 'bullet_fired')"
-          :bullets-hit-player="statStore.getStatByKey(statStore.userSteamStats, index, 'bullet_hit_player')"
-          :headshot="statStore.getStatByKey(statStore.userSteamStats, index, 'headshot')"
+          :kills="statStore.getStatByKey(store, index, 'kill_player')"
+          :deaths="statStore.getStatByKey(store, index, 'deaths')"
+          :bullets-fired="statStore.getStatByKey(store, index, 'bullet_fired')"
+          :bullets-hit-player="statStore.getStatByKey(store, index, 'bullet_hit_player')"
+          :headshot="statStore.getStatByKey(store, index, 'headshot')"
       ></pvp-stats>
+      <farming-stats
+          :scrap="statStore.getStatByKey(store, index, 'acquired_scrap')"
+          :barrels="statStore.getStatByKey(store, index, 'destroyed_barrels')"
+          :leather="statStore.getStatByKey(store, index, 'harvested_leather')"
+          :lgf="statStore.getStatByKey(store, index, 'acquired_lowgradefuel')"
+          :cloth="statStore.getStatByKey(store, index, 'cloth')"
+          :wood="statStore.getStatByKey(store, index, 'wood')"
+          :metal="statStore.getStatByKey(store, index, 'acquired_metal.ore')"
+          :stones="statStore.getStatByKey(store, index, 'stones')"
+      ></farming-stats>
     </div>
   </div>
 </template>
@@ -53,7 +59,12 @@ export default defineComponent({
 <style scoped>
 .user {
   max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
 
+.stats {
   display: flex;
   flex-direction: column;
   gap: 10px;
