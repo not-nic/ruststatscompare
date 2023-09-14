3f8ca06f36ee386191962f6a72c3a5745a700cf3
notnic <nicovo989@gmail.com>
2023-09-14 15:40:15 +0100

    Update GetStats.vue

--- src/components/GetStats.vue
+++ src/components/GetStats.vue
@@ -8,19 +8,57 @@ export default defineComponent({
   data() {
     return {
       statStore: useStatStore(),
-      steamId: null as string
+      steamId: ""
     }
   }
 })
 </script>
 
 <template>
-  <div>
-    <input type="text" v-model="steamId">
-    <input type="submit" @click="statStore.getSteamStats(steamId)"/>
+  <div class="search">
+    <div class="input-container">
+      <input class="input" type="text" placeholder="Enter a steam profile to compare..." v-model="steamId"/>
+      <input class="submit" type="submit" value="Search" disabled v-if="steamId == ''" @click="statStore.getSteamStats(steamId)"/>
+      <input class="submit" type="submit" value="Search" v-else @click="statStore.getSteamStats(steamId)"/>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.search {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
 
+.input-container {
+  min-width: 600px;
+  display: flex;
+  gap: 10px;
+}
+
+input {
+  padding: 15px;
+  background-color: #2a2a2a;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+
+  font-family: 'Space Mono', monospace;
+}
+
+.input {
+  flex: 90%;
+}
+
+.submit {
+  flex: 10%;
+}
+
+.submit:hover {
+  color: #bc3e2b;
+}
 </style>
